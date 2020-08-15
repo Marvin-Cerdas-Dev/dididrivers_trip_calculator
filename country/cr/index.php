94638a7b77e43e54f567b245c1585e577cb6150a
Marvin Cerdas <cer825m@yahoo.com>
2020-08-15 17:39:02 -0600

    Creacion de respositorio, se agrega npm, bootstrap se hacen unos cambios al index de cr

--- country/cr/index.php
+++ country/cr/index.php
@@ -18,19 +18,6 @@
         <meta property="og:description" content="Aquí podras verificar tus viajes de la plataforma Didi de una forma fácil y segura">
         <meta property="og:image" content="http://crdidicalc.azurewebsites.net/images/calculator_128.png">
 
-        <script type="application/ld+json">
-            {
-            "@context": "http://schema.org",
-            "@type": "Organization",
-            "name": "DidiCalc",
-            "url": "https://crdidicalc.azurewebsites.net/",
-            "address": "Costa Rica",
-            "sameAs": [
-            "https://www.facebook.com/calculadoradeplataformasdigitales/"
-            ]
-            }
-        </script>
-
         <!-- Favicon -->
         <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="../../images/favicon-32x32.png">
@@ -38,15 +25,6 @@
         <link rel="manifest" href="../../images/manifest.json">
         <link rel="mask-icon" href="../../safari-pinned-tab.svg" color="#5bbad5">
         <meta name="theme-color" content="#ffffff">
-
-        <!-- Title -->
-        <title>DidiCalc - Calculadora de viajes de Didi</title>
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-        <!-- Default Css Style -->
-        <link href="../../css/default.css" rel="stylesheet">        
 
         <!-- Json-LD -->
         <script type="application/ld+json">
@@ -62,7 +40,15 @@
             "query-input": "required name=search_term_string"
             }
             }
-        </script>
+        </script>        
+
+        <!-- build:css ../../dist/css -->
+        <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link href="../../css/default.css" rel="stylesheet">        
+        <!-- endbuild -->
+
+        <!-- Title -->
+        <title>DidiCalc - Calculadora de viajes de Didi</title>       
 
     </head>
     <!-- Loading page -->
@@ -104,13 +90,40 @@
                 <div class="carousel-item" data-interval="2000">
                     <img src="../../images/carousel_2.jpg" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item" data-interval="2000">
+                <div class="carousel-item">
                     <img src="../../images/carousel_3.jpg" class="d-block w-100" alt="...">
                 </div>
             </div>
         </div>                
         <div class="container-fluid">
             <div class="row">
+                <div class="col-sm-4 card-margin">
+                    <div class="card h-100">
+                        <img class="img-responsive card-img-top" src="../../images/administrate.jpg"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Seguimiento a tus ganancias</h5>
+                            <p class="card-text">Con Didical podras dar seguimiento a tus ganancias de una forma fácil, rápida y segura.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 card-margin">
+                    <div class="card h-100">
+                        <img class="img-responsive card-img-top" src="../../images/free_service.jpg"/>
+                        <div class="card-body">
+                            <h5 class="card-title">100% Gratuito</h5>
+                            <p class="card-text">Didical es una plataroma 100% gratuita, para los socios conductores, por lo que no tendras que preocuparte por cargos mensuales.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 card-margin">
+                    <div class="card h-100">
+                        <img class="img-responsive card-img-top" src="../../images/administrate.jpg"/>
+                        <div class="card-body">
+                            <h5 class="card-title">100% Gratuito</h5>
+                            <p class="card-text">Didical es una plataroma 100% gratuita, para los socios conductores, por lo que no tendras que preocuparte por cargos mensuales.</p>
+                        </div>
+                    </div>
+                </div>
                 <div id="cal_details" class="col-sm-12 col-md-12 col-lg-12">
                     <div class="border rounded padding-container">
                         <center>
@@ -148,16 +161,15 @@
                 </div>
             </div>
         </div>
-
         <footer><?php include_once("../../includes/footer.php") ?></footer>
     </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <!-- Scripst -->
+    <!-- JavaScript -->
+    <!-- build:js ../../dis/js -->
+    <script src="../../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../../node_modules/popper.js/dist/popper.min.js"></script>
+    <script src="../../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../../js/index-scripts.js"></script>
     <script src="../../js/jquery.cookie.js"></script>
+    <script src="../../js/country.js"></script>    
 </body>
 </html>
