ec116991cb78eea4deee2a38472b1f24c470af2a
Marvin Cerdas <cer825m@yahoo.com>
2022-12-23 02:40:35 -0600

    Corrección de texto links sociales

--- country/mx/includes/footer.php
+++ country/mx/includes/footer.php
@@ -5,13 +5,13 @@
                 <a id="facebook" role="button" href="http://www.facebook.com/sharer.php?u=https://crdidicalc.azurewebsites.net/" target="_blank" alt="Compartir en Facebook" title="Facebook" hreflang="es">
                     <img class="socialmedia-img"  src="img/facebook.png" alt="Compartir en Facebook"/>
                 </a>
-                <a id="whatsapp" role="button" href="https://wa.me/?text=Didical%20la%20calculadora%20para%20los%20socios%20conductores%20https://crdidicalc.azurewebsites.net/" target="_blank" alt="Compartir en Whatsapp" title="Compartir en Whatsapp" hreflang="es">
+                <a id="whatsapp" role="button" href="https://wa.me/?text=Didicalc%20la%20calculadora%20para%20los%20socios%20conductores%20https://crdidicalc.azurewebsites.net/" target="_blank" alt="Compartir en Whatsapp" title="Compartir en Whatsapp" hreflang="es">
                     <img class="socialmedia-img"  src="img/whatsapp.png" alt="Compartir en Whatsapp" />
                 </a>   
-                <a id="twitter" role="button" href="http://twitter.com/share?text=Didical%20la%20calculadora%20para%20los%20socios%20conductores%20https://crdidicalc.azurewebsites.net/" target="_blank" alt="Compartir en Twitter" title="Compartir en Twitter" hreflang="es">
+                <a id="twitter" role="button" href="http://twitter.com/share?text=Didicalc%20la%20calculadora%20para%20los%20socios%20conductores%20https://crdidicalc.azurewebsites.net/" target="_blank" alt="Compartir en Twitter" title="Compartir en Twitter" hreflang="es">
                     <img class="socialmedia-img" src="img/twitter.png" alt="Compartir en Twitter"/>
                 </a>              
-                <a id="telegram" role="button" href="https://t.me/share/url?url=https://crdidicalc.azurewebsites.net/&text=Didical%20la%20calculadora%20para%20los%20socios%20conductores" target="_blank" alt="Compartir en Telegram" title="Compartir en Telegram" hreflang="es">
+                <a id="telegram" role="button" href="https://t.me/share/url?url=https://crdidicalc.azurewebsites.net/&text=Didicalc%20la%20calculadora%20para%20los%20socios%20conductores" target="_blank" alt="Compartir en Telegram" title="Compartir en Telegram" hreflang="es">
                     <img class="socialmedia-img" src="img/telegram.png" alt="Compartir en Telegram"/>
                 </a>   
             </div>
